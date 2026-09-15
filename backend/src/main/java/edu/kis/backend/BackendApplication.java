1e3405f4f890d31b4ab940fc5a1dcb8576ad504a
admin <admin@admin.com>
2026-09-15 16:36:12 +0900

    commit

--- backend/src/main/java/edu/kis/backend/BackendApplication.java
+++ backend/src/main/java/edu/kis/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package edu.kis.hello;
+package edu.kis.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHelloApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/edu/kis/backend/BackendApplicationTests.java
+++ backend/src/test/java/edu/kis/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package edu.kis.hello;
+package edu.kis.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootHelloApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
