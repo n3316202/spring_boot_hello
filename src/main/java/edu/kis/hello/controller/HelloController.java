a7681edeb06c4d7efca661ff99d98294dd313d86
admin <admin@admin.com>
2026-09-15 13:41:31 +0900

    commit

--- src/main/java/edu/kis/hello/controller/HelloController.java
+++ src/main/java/edu/kis/hello/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String test() {
-        return "hello world!";
+        return "안녕하세요";
     }
 }
 
